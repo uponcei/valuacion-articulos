c4742a936a5eb35653e4ab782fb109d91fa00af4
Uriel P. Ibarra <uriel.tar.gz@gmail.com>
2023-12-16 21:34:40 -0600

    Proyecto: Valuacion Articulos
    Descripción: se agrega funcionalidad que nos permite  validar los tokens de acceso por medio del Spring Resource Server

---
+++ src/main/java/com/nmp/valuacion/config/ResourceServerConfig.java
@@ -0,0 +1,46 @@
+package com.nmp.valuacion.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @name ResourceServerConfig
+ * @description Clase que nos permite  validar los tokens de acceso y garantizar que el usuario
+*                  que solicita los recursos tenga los permisos necesarios
+ *
+ * @author Uriel P. Ibarra
+ * @version 0.1
+ */
+@Configuration
+@EnableMethodSecurity
+public class ResourceServerConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/swagger-ui/**").permitAll().anyRequest().authenticated())
+                .oauth2ResourceServer(oAuth2 -> oAuth2
+                        .jwt(it -> it.decoder(JwtDecoders.fromIssuerLocation(issuer))))
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
